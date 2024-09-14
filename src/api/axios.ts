06b6892b7295ad8b708fe6336840381fee02f3c2
Idhibhat Pankam <marginpankam@gmail.com>
2024-09-14 16:56:19 +0700

    time out 30s

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -6,5 +6,5 @@ const { API_URL } = config;
 export const apiClient = axios.create({
   baseURL: `${API_URL}/api/v1`,
   headers: { Authorization: `Bearer ${config.API_KEY}` },
-  timeout: 10000,
+  timeout: 30000,
 });
