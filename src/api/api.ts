ffa210cd90e24f51e83640ab097ce3374e611406
Chayanin Kongsareekul <57552210+Aphrodicez@users.noreply.github.com>
2024-09-16 17:34:26 +0700

    Feat: Debt

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,7 @@ import { AxiosResponse } from "axios";
 import { logger } from "../logger/logger";
 import { apiClient } from "./axios";
 import { AddDTO, CompleteChatDTO, ToggleDTO } from "./dto";
+import { number } from "zod";
 
 export const completeChat = async (text: string, guildID: string) => {
   try {
@@ -63,3 +64,37 @@ export const toggleWebSearch = async (guildID: string) => {
     return "Failed to toggle web search";
   }
 };
+
+export const addBorrowMoney = async (borrower: string, lender: string, amount: number, guildID: string) => {
+  try {
+    const res: AxiosResponse<AddDTO> = await apiClient.post(
+      `/guild/${guildID}/debt/add-borrow-money`,
+      {
+        borrower: borrower,
+        lender: lender,
+        amount: amount,
+      }
+    );
+
+    return res.data.reply;
+  } catch (error) {
+    logger.error("Failed to add text", error);
+    return "Failed to add text";
+  }
+};
+
+export const askDebtSummary = async (person: string, guildID: string) => {
+  try {
+    const res: AxiosResponse<CompleteChatDTO> = await apiClient.post(
+      `/guild/${guildID}/debt/ask-debt-summary`,
+      {
+        person: person,
+      }
+    );
+
+    return res.data.reply;
+  } catch (error) {
+    logger.error("Failed to add text", error);
+    return "Failed to add text";
+  }
+}
